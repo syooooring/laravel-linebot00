8d44a9799f171f386b6d63ae76e84c3327c2dd86
syooooring <syooooring@gmail.com>
2020-02-17 11:44:39 +0900

    Update LineBotController.php

--- laravel/app/Http/Controllers/LineBotController.php
+++ laravel/app/Http/Controllers/LineBotController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class LineBotController extends Controller
 {
     public function index()
     {
         return view('linebot.index');
+    }
+
+    public function parrot(Request $request)
+    {
+        Log::debug($request->header());
+        Log::debug($request->input());
     }
 }
