9d384430584a365571f96c726258c2a5c6121b24
syooooring <syooooring@gmail.com>
2020-02-17 21:53:06 +0900

    Update Gurunavi.php

--- laravel/app/Services/Gurunavi.php
+++ laravel/app/Services/Gurunavi.php
@@ -8,6 +8,7 @@ class Gurunavi
 {
 
   private const RESTAURANTS_SEARCH_API_URL = 'https://api.gnavi.co.jp/RestSearchAPI/v3/';
+  //外部APIなどの特定のURLなどを取り扱う時は、定数として定義した上で使うようにすると他のエンジニアに意味が伝わりやすい
 
 
     public function searchRestaurants(string $word): array
@@ -31,5 +32,6 @@ class Gurunavi
         //'keyid'で、env関数を使い、.envファイルのGURUNAVI_ACCESS_KEYの値をセット。
         //'freeword'については、searchRestaurantsメソッドに渡された$wordをstr_replace関数で加工した上でセット。
         //URLとオプションを指定した上でgetメソッドを使い、$responseへぐるなびAPIのレスポンスを代入。
+        //(selfは、自クラス)
     }
 }
