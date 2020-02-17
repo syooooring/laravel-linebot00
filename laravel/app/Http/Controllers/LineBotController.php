9a4798375a287e3c2ee769e7daba02374ef79354
syooooring <syooooring@gmail.com>
2020-02-17 12:04:40 +0900

    Update LineBotController.php

--- laravel/app/Http/Controllers/LineBotController.php
+++ laravel/app/Http/Controllers/LineBotController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use LINE\LINEBot;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+
 
 class LineBotController extends Controller
 {
@@ -21,5 +23,25 @@ class LineBotController extends Controller
 
         $httpClient = new CurlHTTPClient(env('LINE_ACCESS_TOKEN'));
         $lineBot = new LINEBot($httpClient, ['channelSecret' => env('LINE_CHANNEL_SECRET')]);
+
+        $signature = $request->header('x-line-signature');
+        if (!$lineBot->validateSignature($request->getContent(), $signature)) {
+            abort(400, 'Invalid signature');
+        }
+
+        $events = $lineBot->parseEventRequest($request->getContent(), $signature);
+
+        Log::debug($events);
+
+        foreach ($events as $event) {
+            if (!($event instanceof TextMessage)) {
+                Log::debug('Non text message has come');
+                continue;
+            }
+
+            $replyToken = $event->getReplyToken();
+            $replyText = $event->getText();
+            $lineBot->replyText($replyToken, $replyText);
+        }
     }
 }
